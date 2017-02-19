aad93685df1571fb7fd6d870839256504752641b
Daniel Scheller <d.scheller@gmx.net>
2017-02-19 18:08:37 +0100

    copyright notice

--- drivers/media/dvb-frontends/stv0367.c
+++ drivers/media/dvb-frontends/stv0367.c
@@ -7,6 +7,10 @@
  * Copyright (C) 2010,2011 NetUP Inc.
  * Copyright (C) 2010,2011 Igor M. Liplianin <liplianin@netup.ru>
  *
+ * Support for DigitalDevices CineCTv6 and DuoFlex C/T port
+ * (ported from Digital Devices vendor driver package)
+ * Copyright (C) 2017 Daniel Scheller <d.scheller.oss@gmail.com>
+ *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
